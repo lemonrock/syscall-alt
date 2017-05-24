e7cdbf12297cfdee4cd7ae093dffc6e0bd11d206
Raphael Cohn <raphael.cohn@stormmq.com>
2017-05-24 11:02:12 +0100

    Fixes for ARM

--- src/syscalls/linux_like/arm/mod.rs
+++ src/syscalls/linux_like/arm/mod.rs
@@ -2,6 +2,11 @@
 // Copyright © 2016 The developers of syscall-alt. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/syscall-alt/master/COPYRIGHT.
 
 
+use ::SyscallNumber;
+use ::SyscallArgument;
+use ::SyscallResult;
+
+
 include!("syscall0.rs");
 include!("syscall1.rs");
 include!("syscall2.rs");
